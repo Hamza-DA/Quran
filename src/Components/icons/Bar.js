fa266a260d86740af929a7e3c3abcccc3941d261
Hamza Dahmani <60110301+Hamza-DA@users.noreply.github.com>
2021-04-04 22:44:50 +0100

    Delete src directory

--- src/Components/icons/Bar.js
+++
@@ -1,9 +0,0 @@
-const Bar = () => {
-    return(
-        <svg width="68" height="4" viewBox="0 0 68 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line opacity="0.4" x1="1.625" y1="2.375" x2="66.375" y2="2.375" stroke="#11585B" stroke-width="3.25" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    )
-}
-
-export default Bar;
