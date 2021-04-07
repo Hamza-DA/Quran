bee82cfc6960b275445f297763094bae55e05193
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2021-04-07 22:12:57 +0100

    adding animation, loader

--- src/Components/SurahRender.js
+++ src/Components/SurahRender.js
@@ -1,33 +1,47 @@
-import React,{useEffect, useState} from 'react';
-import SurahNameCard from './SurahNameCard';
-import AyahList from './AyahList';
-import Quba from './icons/Quba';
+import React, { useEffect, useState } from "react";
+import SurahNameCard from "./SurahNameCard";
+import AyahList from "./AyahList";
+import Quba from "./icons/Quba";
+import Loading from "./icons/Loading";
+import { motion } from "framer-motion";
 
-function SurahRender({match}) {
-    useEffect(() => {
-        fetchSurah()
-    },[])
+function SurahRender({ match }) {
+  useEffect(() => {
+    fetchSurah();
+  }, []);
 
-    let [Ayah, setAyah] = useState([]);
-    let fetchSurah = async () => {
-        const url = await fetch(`http://api.alquran.cloud/v1/surah/${match.params.id}/ar.alafasy`)
-        const response = await url.json();
-        setAyah(response.data)
-    }
+  let [Ayah, setAyah] = useState([]);
+  let fetchSurah = async () => {
+    const url = await fetch(
+      `http://api.alquran.cloud/v1/surah/${match.params.id}/ar.alafasy`
+    );
+    const response = await url.json();
+    setAyah(response.data);
+  };
 
-    return(
-        <>
-        <SurahNameCard props={Ayah} /> 
-            {Ayah.ayahs && Ayah.ayahs.map(e => {
-                return(
-                    <>
-                    <AyahList props={e} />
-                    </>
-                )
-            })}
-            {!Ayah.ayahs && <></>}
-        </>
-    )
+  return (
+    <>
+      {Ayah.ayahs && (
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <SurahNameCard props={Ayah} />
+        </motion.div>
+      )}
+      {Ayah.ayahs &&
+        Ayah.ayahs.map((e, index) => {
+          return (
+            <>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { delay: index * 0.02 } }}
+              >
+                <AyahList props={e} />
+              </motion.div>
+            </>
+          );
+        })}
+      {!Ayah.ayahs && <Loading />}
+    </>
+  );
 }
 
 export default SurahRender;
--- src/Components/SuratPage.js
+++ src/Components/SuratPage.js
@@ -1,29 +1,43 @@
-import React,{useEffect, useState} from "react";
-import SurahList from './SurahList'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import SurahList from "./SurahList";
+import { Link } from "react-router-dom";
+import Loading from "./icons/Loading";
+import { motion } from "framer-motion";
 
 function SuratPage() {
-    useEffect(() => {
-        fetchSurat()
-    },[])
-    let [Surat, setSurat] = useState([]);
-    let fetchSurat = async () => {
-        const call = await fetch('http://api.alquran.cloud/v1/surah');
-        const back = await call.json()
-        setSurat(back.data)
-    }
-    const mapSurat = Surat.map(e => {
-                return(
-                    <Link to={`/surat/${e.number}`}>
-                        <SurahList key={e.number} latinName={e.englishName} arabicName={e.name} number={e.number} type={e.revelationType} numberOfAyats={e.numberOfAyahs} />
-                    </Link>
-                )
-            })
-    return(
-        <>
-            {mapSurat}
-
-        </>
-    )
+  useEffect(() => {
+    fetchSurat();
+  }, []);
+  let [Surat, setSurat] = useState([]);
+  let fetchSurat = async () => {
+    const call = await fetch("http://api.alquran.cloud/v1/surah");
+    const back = await call.json();
+    setSurat(back.data);
+  };
+  return (
+    <>
+      {Surat &&
+        Surat.map((e, index) => {
+          return (
+            <Link key={e.number} to={`/surat/${e.number}`}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { delay: index * 0.02 } }}
+              >
+                <SurahList
+                  key={e.number}
+                  latinName={e.englishName}
+                  arabicName={e.name}
+                  number={e.number}
+                  type={e.revelationType}
+                  numberOfAyats={e.numberOfAyahs}
+                />
+              </motion.div>
+            </Link>
+          );
+        })}
+      {!Surat.length && <Loading />}
+    </>
+  );
 }
 export default SuratPage;
