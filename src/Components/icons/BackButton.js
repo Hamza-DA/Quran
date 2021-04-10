0e0ac5ba4bc2f18cfdd765f13263bd81d2edd345
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2021-04-10 13:24:23 +0100

    adding icons, return button fixed and animated

--- src/Components/icons/BackButton.js
+++ src/Components/icons/BackButton.js
@@ -1,6 +1,8 @@
-import React from "react";
-import styled from "styled-components";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import Quba from './Quba';
 
 const Button = styled.button`
   border: none;
@@ -9,7 +11,7 @@ const Button = styled.button`
   display: flex;
   align-items: center;
 `;
-const Title = styled.h2`
+const TitleTxt = styled.h2`
   font-family: Nunito Sans;
   font-style: normal;
   font-weight: 600;
@@ -23,28 +25,40 @@ const BackButton = (props) => {
   let history = useHistory();
   return (
     <>
-      <Button
-        onClick={() => {
-          history.goBack();
-        }}
-      >
-        <svg
-          width="9"
-          height="17"
-          viewBox="0 0 9 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+      {props.title && (
+        <motion.span
+          initial={{ opacity: 0, x: '20px' }}
+          animate={{
+            opacity: 1,
+            x: '0',
+            transition: { duration: 0.15 },
+          }}
         >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8.70711 0.792893C9.09763 1.18342 9.09763 1.81658 8.70711 2.20711L2.41421 8.5L8.70711 14.7929C9.09763 15.1834 9.09763 15.8166 8.70711 16.2071C8.31658 16.5976 7.68342 16.5976 7.29289 16.2071L0.292893 9.20711C-0.0976311 8.81658 -0.0976311 8.18342 0.292893 7.79289L7.29289 0.792893C7.68342 0.402369 8.31658 0.402369 8.70711 0.792893Z"
-            fill="#11585B"
-          />
-        </svg>
-        {props.title && <Title>{props.title}</Title>}
-      </Button>
-      {console.log(props)}
+          <Button
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            <svg
+              width='9'
+              height='17'
+              viewBox='0 0 9 17'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M8.70711 0.792893C9.09763 1.18342 9.09763 1.81658 8.70711 2.20711L2.41421 8.5L8.70711 14.7929C9.09763 15.1834 9.09763 15.8166 8.70711 16.2071C8.31658 16.5976 7.68342 16.5976 7.29289 16.2071L0.292893 9.20711C-0.0976311 8.81658 -0.0976311 8.18342 0.292893 7.79289L7.29289 0.792893C7.68342 0.402369 8.31658 0.402369 8.70711 0.792893Z'
+                fill='#11585B'
+              />
+            </svg>
+            {props.title && <TitleTxt>{props.title}</TitleTxt>}
+          </Button>
+        </motion.span>
+      )}
+
+      {!props.title && <Quba />}
     </>
   );
 };
